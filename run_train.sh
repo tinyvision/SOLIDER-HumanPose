8052aba9a92aa4c0a797fadc0d04f8d6e603a72f
哲贤 <xianzhe.xxz@alibaba-inc.com>
2023-07-21 15:45:36 +0800

    add swin backbones and support solider pretrain

---
+++ run_train.sh
@@ -0,0 +1,3 @@
+sh tools/dist_train.sh configs/body/2d_kpt_sview_rgb_img/topdown_heatmap/coco/swin_tiny_coco_384x288_release.py 8
+sh tools/dist_train.sh configs/body/2d_kpt_sview_rgb_img/topdown_heatmap/coco/swin_small_coco_384x288_release.py 8
+sh tools/dist_train.sh configs/body/2d_kpt_sview_rgb_img/topdown_heatmap/coco/swin_base_coco_384x288_release.py 8
